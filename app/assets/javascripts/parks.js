d11982909041860ab2275b14ef5ffac4e14a8a6b
Grady Shelton <gradyish@gmail.com>
2017-07-24 17:45:38 -0700

    see objects in console after ajax call

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,4 +1,5 @@
-
+// PATH GENERATOR
+// var path = d3.geo.path();
 var loadData = function(){
   $.ajax({
     type: 'GET',
@@ -15,13 +16,35 @@ var loadData = function(){
 };
 
 function drawBarPlot(data){
-  console.log('did it get this far?');
-  alert(data);
+  console.log(data);
 }
+
+// var w = 500;
+// var h = 300;
+// //Define path generator, using the Albers USA projection
+// var path = d3.geoPath()
+//        .projection(d3.geoAlbersUsa());
+// //Create SVG element
+// var svg = d3.select("body")
+//       .append("svg")
+//       .attr("width", w)
+//       .attr("height", h);
+// //Load in GeoJSON data
+// d3.json(dataset, function(json) {
+//
+//   //Bind data and create one path per GeoJSON feature
+//   svg.selectAll("path")
+//      .data(json.features)
+//      .enter()
+//      .append("path")
+//      .attr("d", path);
+//
+// });
 
 function error() {
     console.log("Something went wrong!");
 }
+
 
 // fetch data on page load
 $(document).ready(function(){
