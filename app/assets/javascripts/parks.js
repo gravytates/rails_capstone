b027c8458ab94b4d110c130950533e161b1d5cbf
Grady Shelton <gradyish@gmail.com>
2017-07-25 12:01:15 -0700

    produce svg with individual paths on the dom

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,66 +1,68 @@
 var response;
 
 // Ajax call
-var loadData = function(){
-  Promise.resolve(
-    $.ajax({
-      type: 'GET',
-      contentType: 'application/json; charset=utf-8',
-      url: '/parks',
-      dataType: 'json',
-      // timeout: 3000 //3 second timeout
-    })
-  ).then(function(data) {
-    run(data);
-  }).catch(function(e) {
-    if(e.statusText == 'timeout')
-    {
-      alert('Native Promise: Failed from timeout');
-    }
-  });
-};
+// var loadData = function(){
+//   Promise.resolve(
+//     $.ajax({
+//       type: 'GET',
+//       contentType: 'application/json; charset=utf-8',
+//       url: '/parks',
+//       dataType: 'json',
+//       // timeout: 3000 //3 second timeout
+//     })
+//   ).then(function(data) {
+//     run(data);
+//   }).catch(function(e) {
+//     if(e.statusText == 'timeout')
+//     {
+//       alert('Native Promise: Failed from timeout');
+//     }
+//   });
+// };
 
 // width and height for SVG element
-var w = 800;
-var h = 500;
 
-// AlbersUSA projection
-var projection = d3.geoAlbersUsa()
-           .translate([w/2, h/2]);
-          //  .scale([250]);
-//Define path generator
-var path = d3.geoPath()
-       .projection(projection);
-  //Create SVG element
-var svg = d3.select("body")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h)
-//Load in GeoJSON data
-
-function error() {
-    console.log("Something went wrong!");
-}
-
-function run() {
-
-  d3.json('/park_data.json', function(json) {
-    // if (error) {
-    //   console.log(error);
-    // } else {
-    console.log(json);
-    svg.selectAll("div")
-    .data(json.features)
-    .enter()
-    .append("div")
-    .attr("d", path);
-    // }
-
-  });
-}
 
 
 $(document).ready(function(){
   // loadData();
   run();
+  var w = 800;
+  var h = 500;
+
+  // AlbersUSA projection
+  var projection = d3.geoAlbersUsa()
+  .translate([w/2, h/2])
+  .scale([1500]);
+  //  .scale([250]);
+  //Define path generator
+  var path = d3.geoPath(projection);
+  //  .projection(projection);
+  //Create SVG element
+  var svg = d3.select("body")
+  .append("svg")
+  .attr("width", w)
+  .attr("height", h)
+  //Load in GeoJSON data
+
+  function error() {
+    console.log("Something went wrong!");
+  }
+
+  function run() {
+
+    d3.json('/park_data.json', function(json) {
+      // if (error) {
+      //   console.log(error);
+      // } else {
+      console.log(json)
+      svg.selectAll("path")
+      .data(json.features)
+      .enter()
+      .append("path")
+      .attr("d", path);
+      // }
+
+    });
+  }
 });
